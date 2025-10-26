02dfc46b04c97ccae645ef9a4d93d0a1d6a04709
hverma1206 <hv308489@example.com>
2025-10-26 21:50:47 +0530

    improvement acc. to me

--- ncurses_animation.cpp
+++ ncurses_animation.cpp
@@ -1,10 +1,9 @@
 #include <ncurses.h>
 #include <unistd.h>
-
 int main() {
-    int eye_x_left = 10;
-    int eye_x_right = 20;
-    int eye_y = 5;
+    int eye_x_left = 5;
+    int eye_x_right = 18;
+    int eye_y = 3;
     int move_direction = 1;
     int blink_state = 0;
     int frame_count = 0;
@@ -15,63 +14,64 @@ int main() {
     noecho();
     curs_set(0);
     timeout(50);
+    start_color();
+    
+    init_pair(1, COLOR_BLACK, COLOR_WHITE);
+    init_pair(2, COLOR_BLACK, COLOR_CYAN);
 
     getmaxyx(stdscr, max_y, max_x);
 
     while (true) {
         clear();
-
-        if (frame_count % 40 == 0)
-         {
-            blink_state = 1;
-        } 
-        else if (frame_count % 40 == 1) 
+        if (frame_count % 40 == 0) 
         {
+            blink_state = 1;
+        }
+         else if (frame_count % 40 == 1) 
+         {
             blink_state = 2;
         } 
         else if (frame_count % 40 == 2) 
         {
             blink_state = 1;
         } 
-        else if (frame_count % 40 == 3)
-         {
+        else if (frame_count % 40 == 3) 
+        {
             blink_state = 0;
-        }
-
-        if (blink_state == 0) {
-            mvprintw(eye_y, eye_x_left, "( )");
-        } 
-        
-    
-        else if (blink_state == 1) {
-            mvprintw(eye_y, eye_x_left, " - ");
-        } 
-        else {
-            mvprintw(eye_y, eye_x_left, "---");
         }
 
         if (blink_state == 0)
          {
-            mvprintw(eye_y, eye_x_right, "( )");
+            attron(COLOR_PAIR(1));
+            mvprintw(eye_y, eye_x_left, "   ");
+            mvprintw(eye_y + 1, eye_x_left, " o ");
+            mvprintw(eye_y + 2, eye_x_left, "   ");
+            attroff(COLOR_PAIR(1));
+            
+            attron(COLOR_PAIR(1));
+            mvprintw(eye_y, eye_x_right, "   ");
+            mvprintw(eye_y + 1, eye_x_right, " o ");
+            mvprintw(eye_y + 2, eye_x_right, "   ");
+            attroff(COLOR_PAIR(1));
         } 
-        else if
-         (blink_state == 1) {
-            mvprintw(eye_y, eye_x_right, " - ");
+        else if (blink_state == 1) 
+        {
+            mvprintw(eye_y + 1, eye_x_left, "---");
+            mvprintw(eye_y + 1, eye_x_right, "---");
         } 
         else 
         {
-            mvprintw(eye_y, eye_x_right, "---");
-        }
-        mvprintw(eye_y + 1, (eye_x_left + eye_x_right) / 2, "_");
-        refresh();
-        if (getch() == 'q') 
-        {
-            break;
+            mvprintw(eye_y + 1, eye_x_left, "===");
+            mvprintw(eye_y + 1, eye_x_right, "===");
         }
         
-        frame_count++;
-    }
-
+        mvprintw(eye_y + 3, (eye_x_left + eye_x_right) / 2, "^");
+        
+        refresh();
+        if (getch() == 'q') {
+            break;
+        }
+        frame_count++;}
     endwin();
     return 0;
 }
